d57d03fd3b3eb5789ba82a6437999ccb24228e5f
Pengcheng Zhou(Kevin) <monsterzpc@gmail.com>
2019-11-28 13:57:54 -0500

    pow(x, n)

--- 428_Pow(x, n)/pow(x, n).py
+++ 428_Pow(x, n)/pow(x, n).py
@@ -89,3 +89,85 @@ class Solution:
             return tmp_res * tmp_res
         
         return tmp_res * tmp_res * x
+
+
+# Version 3 addition_mul record what part we are missing
+class Solution:
+    """
+    @param x {float}: the base number
+    @param n {int}: the power number
+    @return {float}: the result
+    """
+    
+    """
+    x^n = x^ (n / 2) * x^ (n / 2)
+    n % 2 == 1? x^n = x^(n / 2) * x^ (n / 2) * x
+    
+    res= x
+    (x * x) ^ (n /2)
+    (x ^ 4) ^ (n / 4)
+    
+    4 / 2 = 2, 2/ 2 =1
+    5 /2 = 2, 2/ 2 = 1
+    
+    2 20
+    2 ^2 10
+    2 ^4 5    -> (2^4)^4 * 2 ^ 4 -> 2^4 5
+    2 ^8 2    -> (2^8)^2 * 2^ 4 -> 2^20
+    2 ^16 1
+    
+    """
+    def myPow(self, x, n):
+        # write your code here
+        if n == 0:
+            return 1
+            
+        if x == 0:
+            return 0
+            
+        if n < 0:
+            n = -n 
+            x = 1 / x
+        
+        res = x
+        addition_mul = 1
+        while n > 1:
+            if n % 2 == 1:
+                addition_mul *= res
+            res = res * res
+            n = n // 2
+            
+        return res * addition_mul
+
+class Solution:
+    """
+    @param x {float}: the base number
+    @param n {int}: the power number
+    @return {float}: the result
+    """
+    def myPow(self, x, n):
+        # write your code here
+        if x == 0:
+            return 0
+            
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+            
+        res = 1
+        while n > 0:
+            """
+            every time n shrinks to half of its origin value, x should be squared
+            if n is an odd, we should extrac '1' of current x, and the n becomes even, we just
+            need to multiply the extracted x back to the result
+            
+            """
+            if n % 2 == 1:
+                res *= x
+            x *= x
+            n //= 2
+            
+        return res
