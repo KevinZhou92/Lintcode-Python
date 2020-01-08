844a2e51798e65fb918e4a5a63cb114a21f79e29
Pengcheng Zhou(Kevin) <monsterzpc@gmail.com>
2020-01-08 01:27:58 -0500

    update

--- 12_min-stack/min-stack.py
+++ 12_min-stack/min-stack.py
@@ -1,11 +1,13 @@
 '''
-O(n) for min method
+Use two stacks.
 '''
 class MinStack:
     
     def __init__(self):
         # do intialization if necessary
         self.stack = []
+        # min_stack keeps track of current minimum
+        self.min_stack = []
 
     """
     @param: number: An integer
@@ -14,6 +16,8 @@ class MinStack:
     def push(self, number):
         # write your code here
         self.stack.append(number)
+        if not self.min_stack or self.min_stack[-1] >= number:
+            self.min_stack.append(number)
 
     """
     @return: An integer
@@ -22,12 +26,16 @@ class MinStack:
         # write your code here
         if not self.stack:
             return -1
-            
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        
         return self.stack.pop()
 
     """
     @return: An integer
     """
+    # 1 1 2 3
+    # 1 1 2 3
     def min(self):
         # write your code here
-        return min(self.stack)
+        return self.min_stack[-1]
