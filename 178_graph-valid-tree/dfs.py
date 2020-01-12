8a71c1054806ba0ee4b8ed39ba6b5f05c3863885
Pengcheng Zhou(Kevin) <monsterzpc@gmail.com>
2020-01-12 17:45:32 -0500

    graph valid tree

--- 178_graph-valid-tree/dfs.py
+++ 178_graph-valid-tree/dfs.py
@@ -1,14 +1,6 @@
 '''
-=> BFS
-
+=> DFS
 Time: O(n)
-Space: O(n)
-
-Note:
-1. Undirected Edges, we should build a mutal relationship, a->b and b->a
-2. Valid Tree requires:
-    # of edges = # of nodes - 1
-    can visit all nodes start from 1 node
 '''
 from collections import *
 class Solution:
@@ -19,25 +11,22 @@ class Solution:
     """
     def validTree(self, n, edges):
         # write your code here
-        
         if len(edges) != n - 1:
             return False
         
         neighbors = self.get_neighbors(edges)
-        
-        queue = deque([0])
         visited = set([0])
-        while queue:
-            node = queue.popleft()
-            for neighbor in neighbors[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
-                
+        self.dfs(0, visited, neighbors)
+        
         return len(visited) == n
+    
+    def dfs(self, node, visited, neighbors):
+        for neighbor in neighbors[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                self.dfs(neighbor, visited, neighbors)
         
     def get_neighbors(self, edges):
-        
         neighbors = defaultdict(list)
         
         for edge in edges:
